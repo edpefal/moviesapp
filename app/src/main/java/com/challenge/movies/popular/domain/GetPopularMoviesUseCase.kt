9c3217b606331c9d8fbf2e391ffc554b921efe18
eder <edpefal@gmail.com>
2024-07-13 08:37:24 -0600

    A Use Case for popular movies was added

---
+++ app/src/main/java/com/challenge/movies/popular/domain/GetPopularMoviesUseCase.kt
@@ -0,0 +1,26 @@
+package com.challenge.movies.popular.domain
+
+import com.challenge.movies.popular.data.PopularMovie
+import com.challenge.movies.popular.presentation.PopularMovieModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetPopularMoviesUseCase @Inject constructor(private val popularMoviesRepository: PopularMoviesRepository) {
+
+    suspend fun execute(page: Int): Flow<List<PopularMovieModel>> {
+        return popularMoviesRepository.getPopularMovies(page).map { popularMoviesList ->
+            popularMoviesList.map { it.toPopularMovieModel() }
+        }
+    }
+
+    private fun PopularMovie.toPopularMovieModel(): PopularMovieModel {
+        return PopularMovieModel(
+            id = this.id ?: 0,
+            title = title.orEmpty(),
+            poster = posterPath.orEmpty()
+        )
+    }
+
+
+}
---
+++ app/src/main/java/com/challenge/movies/popular/presentation/PopularMovieModel.kt
@@ -0,0 +1,7 @@
+package com.challenge.movies.popular.presentation
+
+data class PopularMovieModel(
+    val id: Long,
+    val title: String,
+    val poster: String
+)
